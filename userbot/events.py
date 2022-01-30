12d24c32ad83b6af5fe909f59932d8d6b3f44c6f
IndomieGorengSatu <96320576+IndomieGorengSatu@users.noreply.github.com>
2022-01-30 17:28:30 +0700

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -96,11 +96,11 @@ def register(**args):
                 if not disable_errors:
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
-                    text = "**Kyy-Userbot ERROR**\n"
-                    link = "Silahkan chat: @IDnyaKosong"
+                    text = "**IndomieUserbot ERROR**\n"
+                    link = "Silahkan chat: @IndomieGenetik"
                     text += "Untuk melaporkan kesalahan"
-                    text += f"tinggal teruskan pesan ini {link}.\n"
-                    text += "Kyy Siap Membantu Kamu\n"
+                    text += f"teruskan pesan ini {link}.\n"
+                    text += "Bokap Gue Siap Membantu Lo\n"
 
                     ftext = "========== DISCLAIMER =========="
                     ftext += "\nThis file uploaded ONLY here,"
