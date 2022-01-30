b84b96b79c640e46a241b3bae0eb066ce0a70c94
IndomieGorengSatu <96320576+IndomieGorengSatu@users.noreply.github.com>
2022-01-30 19:20:20 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -97,6 +97,9 @@ PMPERMIT_PIC = os.environ.get(
 # Bleep Blop, this is a bot ;)
 PM_AUTO_BAN = sb(os.environ.get("PM_AUTO_BAN", "False"))
 
+# Custom Handler command
+CMD_HANDLER = os.environ.get("CMD_HANDLER") or "."
+
 # Send .chatid in any group with all your administration bots (added)
 G_BAN_LOGGER_GROUP = os.environ.get("G_BAN_LOGGER_GROUP", "")
 if G_BAN_LOGGER_GROUP:
