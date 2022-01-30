58e54dd110d0e21e42f4948ec18fb3021f40f33d
IndomieGorengSatu <96320576+IndomieGorengSatu@users.noreply.github.com>
2022-01-30 17:26:40 +0700

    Update tools.py

--- userbot/utils/tools.py
+++ userbot/utils/tools.py
@@ -219,8 +219,8 @@ async def run_cmd(cmd: list) -> tuple[bytes, bytes]:
 
 def post_to_telegraph(title, html_format_content):
     post_client = TelegraphPoster(use_api=True)
-    auth_name = "Kyy-UserBot"
-    auth_url = "https://github.com/muhammadrizky16/Kyy-UserBot"
+    auth_name = "IndomieUserBot"
+    auth_url = "https://github.com/indomiegorengsatu/IndomieUserBot"
     post_client.create_api_token(auth_name)
     post_page = post_client.post(
         title=title,
