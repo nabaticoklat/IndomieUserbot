07fb51113a0c0d415850dee4e5418b69002864a7
IndomieGorengSatu <96320576+IndomieGorengSatu@users.noreply.github.com>
2022-01-31 19:29:45 +0700

    Update sangmata.py

--- userbot/modules/sangmata.py
+++ userbot/modules/sangmata.py
@@ -4,7 +4,7 @@ from userbot.events import register
 from asyncio.exceptions import TimeoutError
 
 
-@register(outgoing=True, pattern=r"^\.sg(?: |$)(.*)")
+@register(outgoing=True, pattern=r"^\.sa(?: |$)(.*)")
 async def lastname(steal):
     if steal.fwd_from:
         return
@@ -57,5 +57,6 @@ async def lastname(steal):
 
 CMD_HELP.update({
     "sangmata":
-       "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.sg`\
+       "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.sa`\
           \n↳ : Mendapatkan Riwayat Nama Pengguna Yang Di Scan."
+})
