46907fdf257d8ce31000eb31649e342069941cfe
IndomieGorengSatu <96320576+IndomieGorengSatu@users.noreply.github.com>
2022-02-01 06:54:43 +0700

    Update lyrics.py

--- userbot/modules/lyrics.py
+++ userbot/modules/lyrics.py
@@ -17,7 +17,7 @@ if GENIUS is not None:
 
 @register(outgoing=True, pattern="^.lyrics (?:(now)|(.*) - (.*))")
 async def lyrics(lyric):
-    await lyric.edit("`Bentar lagi di ambil mek...`")
+    await lyric.edit("`Getting information...`")
     if GENIUS is None:
         await lyric.edit(
             "`Provide genius access token to Heroku ConfigVars...`")
@@ -26,7 +26,7 @@ async def lyrics(lyric):
         playing = User(LASTFM_USERNAME, lastfm).get_now_playing()
         if playing is None:
             await lyric.edit(
-                "`Gue Ga Nemu Apapun di Lastfm scrobbling cuy...`"
+                "`No information current lastfm scrobbling...`"
             )
             return False
         artist = playing.get_artist()
@@ -34,16 +34,15 @@ async def lyrics(lyric):
     else:
         artist = lyric.pattern_match.group(2)
         song = lyric.pattern_match.group(3)
-    await lyric.edit(f"`Sedang Mencari {artist} - {song}...`")
+    await lyric.edit(f"`Searching lyrics for {artist} - {song}...`")
     songs = genius.search_song(song, artist)
     if songs is None:
-        await lyric.edit(f"`Lagu`  **{artist} - {song}**  `Tidak Ditemukan...`")
+        await lyric.edit(f"`Song`  **{artist} - {song}**  `not found...`")
         return False
     if len(songs.lyrics) > 4096:
-        await lyric.edit("`Lirik nya Terlalu Panjang, Liat Di File Yang Gua kirim Aja.`")
+        await lyric.edit("`Lyrics is too big, view the file to see it.`")
         with open("lyrics.txt", "w+") as f:
-            f.write(
-                f"Permintaan Pencarian: \n{artist} - {song}\n\n{songs.lyrics}")
+            f.write(f"Search query: \n{artist} - {song}\n\n{songs.lyrics}")
         await lyric.client.send_file(
             lyric.chat_id,
             "lyrics.txt",
@@ -53,7 +52,7 @@ async def lyrics(lyric):
         return True
     else:
         await lyric.edit(
-            f"**Permintaan Pencarian**:\n`{artist}` - `{song}`"
+            f"**Search query**:\n`{artist}` - `{song}`"
             f"\n\n```{songs.lyrics}```"
         )
         return True
@@ -61,8 +60,8 @@ async def lyrics(lyric):
 
 CMD_HELP.update({
     "lyrics":
-    "`.lyrics` **<nama artis> - <judul lagu>**"
-    "\nUsage: Mendapatkan Lirik Yang Sesuai Dengan Artis Dan Lagu."
+    "`.lyrics` **<artist name> - <song name>**"
+    "\nUsage: Get lyrics matched artist and song."
     "\n\n`.lyrics now`"
-    "\nUsage: Mendapatkan Lirik Dan Artis Dari lastfm scrobbling Saat Ini."
+    "\nUsage: Get lyrics artist and song from current lastfm scrobbling."
 })
