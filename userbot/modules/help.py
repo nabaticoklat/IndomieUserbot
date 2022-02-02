547e6692a3efc110bd1613bc6d1ccd8d53fa52c5
IndomieGorengSatu <rezagaming61@gmail.com>
2022-02-02 00:38:07 +0000

    pylint: auto fixes
    Signed-off-by: IndomieGorengSatu <rezagaming61@gmail.com>

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -34,9 +34,9 @@ async def help(event):
             string += "`" + str(i)
             string += "`\t ✦  "
         await event.edit(
-                         f"**♨ List Help [ɪɴᴅᴏᴍɪᴇᴜꜱᴇʀʙᴏᴛ] (https://github.com/indomiegorengsatu/IndomieUserbot)**\n"
-                         f"**♨ ʙᴏᴛ ᴏᴡɴᴇʀ {DEFAULTUSER}**\n**♨ Mᴏᴅᴜʟᴇꜱ : {len(modules)}**\n\n"
-                         "**• Mᴀɪɴ Mᴇɴᴜ :**\n"
-                         f"✦ {string}✦\n\n✐ **ɴᴏᴛᴇꜱ :** <`.help ping`> Untuk Informasi Pengunaan.\nJangan Lupa Berdoa Sebelum Mencoba wahahaha..")
+            f"**♨ List Help [ɪɴᴅᴏᴍɪᴇᴜꜱᴇʀʙᴏᴛ] (https://github.com/indomiegorengsatu/IndomieUserbot)**\n"
+            f"**♨ ʙᴏᴛ ᴏᴡɴᴇʀ {DEFAULTUSER}**\n**♨ Mᴏᴅᴜʟᴇꜱ : {len(modules)}**\n\n"
+            "**• Mᴀɪɴ Mᴇɴᴜ :**\n"
+            f"✦ {string}✦\n\n✐ **ɴᴏᴛᴇꜱ :** <`.help ping`> Untuk Informasi Pengunaan.\nJangan Lupa Berdoa Sebelum Mencoba wahahaha..")
         await asyncio.sleep(1000)
         await event.delete()
