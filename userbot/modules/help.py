e122ef432343872e039d8e2e2725e3a4ba305927
IndomieGorengSatu <96320576+IndomieGorengSatu@users.noreply.github.com>
2022-02-02 22:38:37 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -33,8 +33,7 @@ async def help(event):
         for i in CMD_HELP:
             string += "`" + str(i)
             string += "`\t ✦  "
-        await event.edit(
-            f"**♨ List Help [ɪɴᴅᴏᴍɪᴇᴜꜱᴇʀʙᴏᴛ] (https://github.com/indomiegorengsatu/IndomieUserbot)**\n"
+        await event.edit("**♨ List Help [ɪɴᴅᴏᴍɪᴇᴜꜱᴇʀʙᴏᴛ] **\n\n"
             f"**♨ ʙᴏᴛ ᴏᴡɴᴇʀ {DEFAULTUSER}**\n**♨ Mᴏᴅᴜʟᴇꜱ : {len(modules)}**\n\n"
             "**• Mᴀɪɴ Mᴇɴᴜ :**\n"
             f"✦ {string}✦\n\n✐ **ɴᴏᴛᴇꜱ :** <`.help ping`> Untuk Informasi Pengunaan.\nJangan Lupa Berdoa Sebelum Mencoba wahahaha..")
