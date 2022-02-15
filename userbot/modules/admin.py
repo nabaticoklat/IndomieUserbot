639fe9c20b1c13a40efdfd878ba78e49fe0cd299
Indomiee <96320576+IndomieGorengSatu@users.noreply.github.com>
2022-02-15 17:11:30 +0700

    Update admin.py

--- userbot/modules/admin.py
+++ userbot/modules/admin.py
@@ -76,7 +76,6 @@ MUTE_RIGHTS = ChatBannedRights(until_date=None, send_messages=True)
 UNMUTE_RIGHTS = ChatBannedRights(until_date=None, send_messages=False)
 # ================================================
 
-
 @register(outgoing=True, pattern=r"^\.setgpic$")
 async def set_group_photo(gpic):
     if not gpic.is_group:
