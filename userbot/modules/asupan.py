eb15ef752623958d9e962d002fbf460363535b10
Indomiee <96320576+IndomieGorengSatu@users.noreply.github.com>
2022-03-11 22:11:17 +0700

    Update asupan.py

--- userbot/modules/asupan.py
+++ userbot/modules/asupan.py
@@ -2,11 +2,13 @@
 # ⚠️ Do not remove credits
 
 
+
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP
 from userbot.events import register
 import random
-from telethon.tl.types import InputMessagesFilterVideo
+from userbot import owner
+from telethon.tl.types import InputMessagesFilterVideo, InputMessagesFilterVoice
 
 
 @register(outgoing=True, pattern=r"^\.asupan$")
@@ -15,7 +17,7 @@ async def _(event):
         asupannya = [
             asupan
             async for asupan in event.client.iter_messages(
-                "@Gabutnyazaen", filter=InputMessagesFilterVideo
+                "@tedeasupancache", filter=InputMessagesFilterVideo
             )
         ]
         await event.client.get_me()
@@ -24,9 +26,29 @@ async def _(event):
             file=random.choice(asupannya),
             caption=f"**Berhasil menemukan Video**.")
 
-await event.edit()
-except Exception:
-    await event.edit("Tidak bisa menemukan video asupan.")
+        await event.edit()
+    except Exception:
+        await event.edit("Tidak bisa menemukan video asupan.")
+
+
+@register(outgoing=True, pattern=r"^\.desah$")
+async def _(event):
+    try:
+        asupannya = [
+            asupan
+            async for asupan in event.client.iter_messages(
+                "@DESAHANFCE", filter=InputMessagesFilterVoice
+            )
+        ]
+        await event.client.get_me()
+        await event.client.send_file(
+            event.chat_id,
+            file=random.choice(asupannya),
+            caption=f"**Nih kak [{owner}](tg://user?id={chat.id}) Asupannya**.")
+
+        await event.edit()
+    except Exception:
+        await event.edit("Tidak bisa menemukan video asupan.")
 
 
 CMD_HELP.update(
@@ -34,6 +56,8 @@ CMD_HELP.update(
         "asupan": f"**Plugin : **`asupan`\
         f\n\n  •  **Syntax :** `{cmd}asupan`\
         \n  •  **Function : **Untuk Mengirim video asupan secara random.\
+        f\n\n  •  **Syntax :** `{cmd}desah`\
+        \n  •  **Function : **Untuk Mengirim voice desah secara random.\
     "
     }
 )
