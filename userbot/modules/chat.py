e6b0da70221f5ebd5984ca8dc6eb8ea2c567de40
Indomiee <96320576+IndomieGorengSatu@users.noreply.github.com>
2022-03-13 15:26:12 +0700

    Update chat.py

--- userbot/modules/chat.py
+++ userbot/modules/chat.py
@@ -34,9 +34,9 @@ async def _(event):
             bot_api_file_id = pack_bot_file_id(r_msg.media)
             await event.edit("ID Grup: `{}`\nID Dari Pengguna : `{}`\nID Bot File API: `{}`".format(str(event.chat_id), str(r_msg.from_id), bot_api_file_id))
         else:
-            await event.edit("ID Grup: `{}`\nID Dari Pengguna : `{}`".format(str(r_msg.from_id)))
+            await event.edit("ID Grup: `{}`\nID Dari Pengguna : `{}`".format(str(event.chat_id), str(r_msg.from_id)))
     else:
-        await event.edit("ID Pengguna : `{}`".format(str(event.chat_id)))
+        await event.edit("ID Grup: `{}`".format(str(event.chat_id)))
 
 
 @register(outgoing=True, pattern="^.link(?: |$)(.*)")
