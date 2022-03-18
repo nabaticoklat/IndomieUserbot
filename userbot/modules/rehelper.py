e61cc83f668373bea29654930a414d4d182bc1a0
Indomiee <96320576+IndomieGorengSatu@users.noreply.github.com>
2022-03-18 23:18:00 +0700

    Indomie Userbot

--- userbot/modules/rehelper.py
+++ userbot/modules/rehelper.py
@@ -1,33 +1,36 @@
 """ Userbot module for other small commands. """
-from userbot import CMD_HELP, ALIVE_NAME
-from userbot.events import register
+from userbot import CMD_HELP
+from userbot import CMD_HANDLER as cmd
+from userbot.utils import indomie_cmd
 
 
-# ================= CONSTANT =================
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
-# ============================================
 
-
-@register(outgoing=True, pattern="^.lhelp$")
+@register(pattern="lhelp$")
 async def usit(e):
+    aku = await e.client.get_me()
     await e.edit(
-        f"**Halo {DEFAULTUSER} Jika Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke:\n"
-        "\n[Telegram](t.me/IndomieGenetik)"
-        "\n[Repo](https://github.com/indomiegorengsatu/IndomieUserbot)"
-        "\n[Instagram](instagram.com/w1thmyluv)")
+        f"**Halo [{aku.first_name}](tg://user?id={aku.id}) Jika Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke:\n"
+        "\n┌ [Indomie](t.me/IndomieGenetik)"
+        "\n├ [Support](t.me/IndomieProject)"
+        "\n├ [IndomieStore](t.me/IndomieStore) `Jasa Bikin Bot.`"
+        "\n├ [Repo](https://github.com/indomiegorengsatu/IndomieUserbot)"
+        "\n└ [Instagram](instagram.com/w1thmyluv)")
 
 
-@register(outgoing=True, pattern="^.vars$")
+@indomie_cmd(pattern="vars$")
 async def var(m):
     await m.edit(
-        f"**Disini Daftar Vars Dari {DEFAULTUSER}:**\n"
-        "\n[DAFTAR VARS](https://raw.githubusercontent.com/IndomieGorengSatu/IndomieUserbot/IndomieUserbot/varshelper.txt)")
+        f"**Ini Daftar Vars Dari [IndomieUserbot](https://github.com/indomiegorengsatu/IndomieUserbot):**\n"
+        "\n┌ [DAFTAR VARS](https://raw.githubusercontent.com/IndomieGorengSatu/IndomieUserbot/IndomieUserbot/varshelper.txt)"
+        "\n├ [Support](t.me/IndomieProject)"
+        "\n├ [IndomieStore](t.me/IndomieStore) `Jasa Bikin Bot.`"
+        "\n└ [Repo](https://github.com/indomiegorengsatu/IndomieUserbot)")
 
 
 CMD_HELP.update({
-    "helper":
-    "`.lhelp`\
-\nUsage: Bantuan Untuk IndomieUserbot.\
-\n`.vars`\
-\nUsage: Melihat Daftar Vars."
+    "rehelper":
+    f"𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}lhelp`\
+    \n↳ : Meminta Bantuan.\
+    \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}vars` :\
+    \n↳ : Melihat vars dari `IndomieUserbot`."
 })
