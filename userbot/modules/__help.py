439214356bab620350c9cedbcb41027a952b478c
Indomiee <96320576+IndomieGorengSatu@users.noreply.github.com>
2022-03-18 15:21:44 +0700

    Indomie userbot

--- userbot/modules/__help.py
+++ userbot/modules/__help.py
@@ -1,28 +1,34 @@
-import logging
+from userbot import BOT_USERNAME, CMD_HELP, bot, user
+from userbot.utils import edit_delete, edit_or_reply, skyzu_cmd
 
-from userbot import BOT_USERNAME
-from userbot.events import register
-
-logging.basicConfig(
-    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s",
-    level=logging.WARNING)
+DEFAULTUSER = user.first_name
+CUSTOM_HELP_EMOJI = "✨"
 
 
-@register(outgoing=True, pattern=r"^\.helpme")
-async def yardim(event):
-    try:
-        tgbotusername = BOT_USERNAME
-        if tgbotusername is not None:
-            results = await event.client.inline_query(tgbotusername, "@IndomieGenetik")
+@skyzu_cmd(pattern="help ?(.*)")
+async def cmd_list(event):
+    args = event.pattern_match.group(1).lower()
+    if args:
+        if args in CMD_HELP:
+            await edit_or_reply(
+                event,
+                f"**✘ Commands available in {args} ✘** \n\n"
+                + str(CMD_HELP[args])
+                + "\n\n**☞ @IndomieProject**",
+            )
+        else:
+            await edit_delete(event, f"**Module** `{args}` **Tidak tersedia!**")
+    else:
+        try:
+            results = await bot.inline_query(  # pylint:disable=E0602
+                BOT_USERNAME, "@IndomieUserbot"
+            )
             await results[0].click(
                 event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
             )
             await event.delete()
-        else:
-            await event.edit(
-                "`Botnya tidak berfungsi! Silahkan atur Bot Token dan Username dengan benar. Modul telah dihentikan.`"
+        except BaseException:
+            await edit_delete(
+                event,
+                f"** Sepertinya obrolan atau bot ini tidak mendukung inline mode.**",
             )
-    except Exception:
-        return await event.edit(
-            "`Anda tidak dapat mengirim hasil sebaris dalam hal ini ke chat (disebabkan oleh Mengirim Inline Sebaris)\ngunakan perintah .inlineon`"
-        )
