229b42a0f7b0d2e332fef7d0a1467c43570d56a4
Indomiee <96320576+IndomieGorengSatu@users.noreply.github.com>
2022-03-19 13:12:59 +0700

    Indomie userbot

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -110,22 +110,6 @@ while 0 < 6:
 
 del _DEVS
 
-# Jangankan yang ini, yang atas aja ga blh apalagi yang ini kontol
-# Blacklist User for IndomieUserbot
-while 0 < 6:
-    _BLACKLIST = get(
-        "https://raw.githubusercontent.com/IndomieGorengSatu/Mie/master/mieblacklist.json"
-    )
-    if _BLACKLIST.status_code != 200:
-        if 0 != 5:
-            continue
-        mieblacklist = []
-        break
-    mieblacklist = _BLACKLIST.json()
-    break
-
-del _BLACKLIST
-
 SUDO_USERS = {
     int(x) for x in os.environ.get(
         "SUDO_USERS",
@@ -370,6 +354,22 @@ API_URL = os.environ.get("API_URL", "http://antiddos.systems")
 # Inline bot helper
 BOT_TOKEN = os.environ.get("BOT_TOKEN") or None
 BOT_USERNAME = os.environ.get("BOT_USERNAME") or None
+
+# Jangankan yang ini, yang atas aja ga blh apalagi yang ini kontol
+# Blacklist User for IndomieUserbot
+while 0 < 6:
+    _BLACKLIST = get(
+        "https://raw.githubusercontent.com/IndomieGorengSatu/Mie/master/mieblacklist.json"
+    )
+    if _BLACKLIST.status_code != 200:
+        if 0 != 5:
+            continue
+        mieblacklist = []
+        break
+    mieblacklist = _BLACKLIST.json()
+    break
+
+del _BLACKLIST
 
 # Init Mongo
 MONGOCLIENT = MongoClient(MONGO_URI, 27017, serverSelectionTimeoutMS=1)
