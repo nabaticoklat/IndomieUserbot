d5f0ef393b7ec3f8df8bd2d994494a526d347dce
Indomiee <96320576+IndomieGorengSatu@users.noreply.github.com>
2022-03-20 01:00:39 +0700

    Indomie userbot

--- userbot/modules/toxic.py
+++ userbot/modules/toxic.py
@@ -1,145 +1,142 @@
-from platform import uname
-from userbot import ALIVE_NAME, CMD_HELP
-from userbot.events import register
-
-# ================= CONSTANT =================
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
-# ============================================
+from userbot import CMD_HELP
+from userbot import CMD_HANDLER as cmd
+from userbot.utils import indomie_cmd
 
 
-@register(outgoing=True, pattern='^.d(?: |$)(.*)')
+
+@indomie_cmd(pattern='d(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("**BACOT NGENTOTTT!!BAPA LU SURUH RIBUT SAMA GUA**")
 
 
-@register(outgoing=True, pattern='^.e(?: |$)(.*)')
+@indomie_cmd(pattern='e(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("**GAK USAH SOK KERAS GOBLOK!!KENCING MASIH BERDIRI AJA BELAGU**")
 
 
-@register(outgoing=True, pattern='^.f(?: |$)(.*)')
+@indomie_cmd(pattern='f(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("**MUKA LU SEMUA KAYA KONTOL HAHAHAHA!!**")
 
 
-@register(outgoing=True, pattern='^.i(?: |$)(.*)')
+@indomie_cmd(pattern='i(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("**KONTOL MASIH BENGKOK AJA BANGGA LU HAHAHAHA!!**")
 
 
-@register(outgoing=True, pattern='^.r(?: |$)(.*)')
+@indomie_cmd(pattern='r(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("**KONTOL KONTOL APA YANG BESAR?KONTOL LU LAH HAHAHAHA!!**")
 
 
-@register(outgoing=True, pattern='^.t(?: |$)(.*)')
+@indomie_cmd(pattern='t(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("**BABI!!KONTOL!!NGENTOT!!!**")
 
 
-@register(outgoing=True, pattern='^.u(?: |$)(.*)')
+@indomie_cmd(pattern='u(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("**BABI LU GOBLOK!!GANTENGAN JUGA GUA HAHAHAHA**")
 
 
-@register(outgoing=True, pattern='^.w(?: |$)(.*)')
+@indomie_cmd(pattern='w(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("**BABI LU GOBLOK!!CANTIKAN JUGA GUA HAHAHAHA**")
 
 
-@register(outgoing=True, pattern='^.z(?: |$)(.*)')
+@indomie_cmd(pattern='z(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("**BACOTAN LU GAK BIKIN GUA TREMOR GOBLOK HAHAHAHA!!**")
 
 
-@register(outgoing=True, pattern='^.k(?: |$)(.*)')
+@indomie_cmd(pattern='k(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("**HAI PERKENALKAN NAMA SAYA GAK TAU LUPA!!**")
 
 
-@register(outgoing=True, pattern='^.n(?: |$)(.*)')
+@indomie_cmd(pattern='n(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("**GIMANA KABAR KAMU SAYANG??APAKAH BAIK?**")
 
 
-@register(outgoing=True, pattern='^.b(?: |$)(.*)')
+@indomie_cmd(pattern='b(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("**ELEEEHHHH SOK BANGET KEPINTERAN KAMU!!**")
 
 
-@register(outgoing=True, pattern='^.m(?: |$)(.*)')
+@indomie_cmd(pattern='m(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("**INI GRUB APA KUBURAN SEPI BANGET ASTAGFIRULLAH!!**")
 
 
-@register(outgoing=True, pattern='^.c(?: |$)(.*)')
+@indomie_cmd(pattern='c(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("**KAN UDAH GUA BILANG??MAKANYA JANGAN NGEYEL!!**")
 
 
-@register(outgoing=True, pattern='^.s(?: |$)(.*)')
+@indomie_cmd(pattern='s(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("**GAUSAH SOKAP DEH KAMU!!**")
 
 
-@register(outgoing=True, pattern='^.v(?: |$)(.*)')
+@indomie_cmd(pattern='v(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("**MACAM BAGUS AE LU BEGITU HMM!!**")
 
 
-@register(outgoing=True, pattern='^.j(?: |$)(.*)')
+@indomie_cmd(pattern='j(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("**MAAF BUKAN JAGOAN HAHAHAHA!!**")
 
 
-@register(outgoing=True, pattern='^.a(?: |$)(.*)')
+@indomie_cmd(pattern='a(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("**BISMILLAH SLEEP CALL!!😁**")
 
 
-@register(outgoing=True, pattern='^.g(?: |$)(.*)')
+@indomie_cmd(pattern='g(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("**GANTENG LU BEGITU???**")
 
 
-@register(outgoing=True, pattern='^.y(?: |$)(.*)')
+@indomie_cmd(pattern='y(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("**War War Tai anjing, Ketrigger minta sharelok, Udah di sharelok Ga nyamperin,Keras di sosmed Bhakss...**")
 
 
-@register(outgoing=True, pattern='^.h(?: |$)(.*)')
+@indomie_cmd(pattern='h(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("**CANTIK LU BEGITU???**")
 
 
-@register(outgoing=True, pattern='^.o(?: |$)(.*)')
+@indomie_cmd(pattern='o(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("**MENTANG MENTANG PUNYA BOT MAINNYA BOT!!PANTES MUKANYA KAYA BOT😁**")
 
 
-@register(outgoing=True, pattern='^.norak(?: |$)(.*)')
+@indomie_cmd(pattern='norak(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit("**NORAK LU KONTOL!! MAKE USERBOT CUMAN BUAT WAR MALAH SOK KERAS**")
@@ -147,50 +144,50 @@ async def typewriter(typew):
 
 CMD_HELP.update({
     "toxic":
-    ".d\
+    f"{cmd}d\
 \nUsage: Bacotin Orang.\
 \n\n.e\
 \nUsage: Buat Orang Yang Sok Keras.\
-\n\n.f\
+\n\n{cmd}f\
 \nUsage: Ngatain Orang Wkwkkw.\
-\n\n.i\
+\n\n{cmd}i\
 \nUsage: Kontol Orang Ngatain.\
-\n\n.r\
+\n\n{cmd}r\
 \nUsage: Pantun Anjing.\
-\n\n.t\
+\n\n{cmd}t\
 \nUsage: Nyebutin Binatang.\
-\n\n.u\
+\n\n{cmd}u\
 \nUsage: Biar Dikata Ganteng.\
-\n\n.w\
+\n\n{cmd}w\
 \nUsage: Biar Dikata Cantik.\
-\n\n.z\
+\n\n{cmd}z\
 \nUsage: Tremor Kan Lu.\
-\n\n.k\
+\n\n{cmd}k\
 \nUsage: Memperkenalkan Diri.\
-\n\n.n\
+\n\n{cmd}n\
 \nUsage: Menanyakan Kabar.\
-\n\n.b\
+\n\n{cmd}b\
 \nUsage: Sok Kepinteran.\
-\n\n.m\
+\n\n{cmd}m\
 \nUsage: Gc Nya Kaya kuburan.\
-\n\n.c\
+\n\n{cmd}c\
 \nUsage: Dia tuh Ngeyel banget.\
-\n\n.s\
+\n\n{cmd}s\
 \nUsage: Haha sokap.\
-\n\n.v\
+\n\n{cmd}v\
 \nUsage: Merendah.\
-\n\n.a\
+\n\n{cmd}a\
 \nUsage: Nyari Sleep Call.\
-\n\n.j\
+\n\n{cmd}j\
 \nUsage: Hujat yang gapunya muka.\
-\n\n.g\
+\n\n{cmd}g\
 \nUsage: Kegantengan.\
-\n\n.y\
+\n\n{cmd}y\
 \nUsage: teruntuk petarung.\
-\n\n.h\
+\n\n{cmd}h\
 \nUsage: Kecantikan.\
-\n\n.o\
+\n\n{cmd}o\
 \nUsage: Ngatain org norak.\
-\n\n.norak\
+\n\n{cmd}norak\
 \nUsage: Ngatain Petarunx Tele."
 })
