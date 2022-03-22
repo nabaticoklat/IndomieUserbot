412330d1d36a849b4bc1d5000f16cb34611f3a17
IndomieGorengSatu <rezagaming61@gmail.com>
2022-03-22 06:54:04 +0000

    pylint: auto fixes
    Signed-off-by: IndomieGorengSatu <rezagaming61@gmail.com>

--- userbot/modules/owner.py
+++ userbot/modules/owner.py
@@ -19,7 +19,7 @@ from userbot import BOTLOG_CHATID
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP, DEVS
 from userbot.utils import edit_or_reply
-from userbot.events import register 
+from userbot.events import register
 from userbot.modules.ping import absen
 
 from .admin import BANNED_RIGHTS, UNBAN_RIGHTS
